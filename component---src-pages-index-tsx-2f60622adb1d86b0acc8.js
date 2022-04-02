abcc8731f5499932b25ff957c6ecacf541e3a0e4
Harikrishnan N <harikrishnan.nambbothiri@eventinc.de>
2022-04-02 12:02:00 +0530

    Updates

---
+++ component---src-pages-index-tsx-2f60622adb1d86b0acc8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkharisr92_github_io=self.webpackChunkharisr92_github_io||[]).push([[691],{4730:function(e,t,a){a.r(t);var i=a(7294);t.default=function(e){var t=e.data.site;return i.createElement("main",null,i.createElement("h1",null,t.siteMetadata.title),i.createElement("h2",null,t.siteMetadata.description))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-2f60622adb1d86b0acc8.js.map
