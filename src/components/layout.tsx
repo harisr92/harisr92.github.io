0dc35aa8115cd84be10a2584a6db25be73c1f89a
Harikrishnan N <harikrishnan.nambbothiri@eventinc.de>
2022-04-13 21:39:11 +0530

    remove unwanted styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,6 @@ import { css } from "@emotion/core"
 import { Link } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Aside from "./Aside/Aside";
-import "react-pro-sidebar/dist/css/styles.css";
-import "./Aside/Aside.scss";
 import "./layout.css";
 
 const Layout: React.FC = ({ children }) => {
